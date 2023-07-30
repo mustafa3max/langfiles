f871ee8e88aeb6a9f285210ba1f2d614172265e7
mustafamax <max@max.com>
2023-07-30 23:30:34 +0300

    commit 21

--- resources/views/components/editor/code.blade.php
+++ resources/views/components/editor/code.blade.php
@@ -1,6 +1,6 @@
 <div class="py-2 w-fit mx-auto">
-    <div class="bg-secondary-light dark:bg-secondary-dark p-2 rounded-lg" id="{{ $id }}"
-        onclick="selectText('{{ $id }}')">
-        <pre dir="ltr"><code>{{ $slot }}</code></pre>
+    <div class="bg-secondary-light dark:bg-secondary-dark p-2 rounded-lg flex flex-wrap overflow-hidden"
+        id="{{ $id }}" onclick="selectText('{{ $id }}')">
+        <code dir="ltr" class="whitespace-pre-wrap p-4">{{ $slot }}</code>
     </div>
 </div>
